82905dca1217e6faeba01fb98a54e4eaf31c8c7b
Adam <lehreradam@yahoo.com>
2022-05-10 09:27:53 -0700

    static js

--- server/server.js
+++ server/server.js
@@ -52,9 +52,9 @@ if(process.env.NODE_ENV === "development"){
     console.log("here fefe")
     app.use(express.static(path.resolve(__dirname, '../client/build')));
     app.get('/*', function (req, res) {
-        res.sendFile(path.resolve(__dirname, '../client/build',"index.html"));
+        res.sendFile(path.join(__dirname, '../client/build',"index.html"));
     });
-}
+} 
   
  
 app.use("/users", userRouter); 
