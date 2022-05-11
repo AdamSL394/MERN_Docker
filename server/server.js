fe56db9c25f8319270fe78a1a6bb550f7e0701f0
Adam <lehreradam@yahoo.com>
2022-05-11 13:14:23 -0700

    remove one app.use in server

--- server/server.js
+++ server/server.js
@@ -88,7 +88,7 @@ app.use(cors());
 
 
 if(process.env.NODE_ENV === "development"){
-    app.use('/static',express.static(path.resolve(__dirname, '../client/build')));
+    //app.use('/static',express.static(path.resolve(__dirname, '../client/build')));
     app.get('/*', function (req, res) {
         res.sendFile(path.join(__dirname, '../client/build',"index.html"));
     });
