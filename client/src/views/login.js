5da94218f2d534acd19fda3befa0ae25f06a2ca9
Adam <lehreradam@yahoo.com>
2022-05-11 14:04:26 -0700

    added animation check memory leak

--- client/src/views/login.js
+++ client/src/views/login.js
@@ -8,57 +8,57 @@ function Login() {
 
   const [text, setText] = useState()
 
-  // useEffect(() => {
-  //   var words = ['Note Script'],
-  //   part,
-  //   i = 0,
-  //   offset = 0,
-  //   len = words.length,
-  //   forwards = true,
-  //   skip_count = 0,
-  //   skip_delay = 15,
-  //   speed = 100;
+  useEffect(() => {
+    var words = ['Note Script'],
+    part,
+    i = 0,
+    offset = 0,
+    len = words.length,
+    forwards = true,
+    skip_count = 0,
+    skip_delay = 15,
+    speed = 100;
 
-  //     let count = 0
-  //     let a = setInterval(function () {
+      let count = 0
+      let a = setInterval(function () {
   
-  //       if (forwards) {
-  //         if (offset >= words[i].length) {
-  //           ++skip_count;
-  //           if (skip_count === skip_delay) {
-  //             forwards = false;
-  //             skip_count = 0;
-  //           }
-  //         }
-  //       }
-  //       else {
-  //         if (offset === 0) {
-  //           forwards = true;
-  //           i++;
-  //           offset = 0;
-  //           if (i >= len) {
-  //             i = 0;
-  //           }
-  //         }
-  //       }
-  //       part = words[i].substr(0, offset);
-  //       if (skip_count === 0) {
-  //         if (forwards) {
-  //           offset++;
-  //         }
-  //         else {
-  //           offset--;
-  //         }
-  //       }
-  //       setText(part);
-  //       count++
-  //       //37 script disappears
-  //       //25 keeps text on screen
-  //       if (count === 25) {
-  //         clearInterval(a)
-  //       }
-  //     }, speed);
-  // }, [])
+        if (forwards) {
+          if (offset >= words[i].length) {
+            ++skip_count;
+            if (skip_count === skip_delay) {
+              forwards = false;
+              skip_count = 0;
+            }
+          }
+        }
+        else {
+          if (offset === 0) {
+            forwards = true;
+            i++;
+            offset = 0;
+            if (i >= len) {
+              i = 0;
+            }
+          }
+        }
+        part = words[i].substr(0, offset);
+        if (skip_count === 0) {
+          if (forwards) {
+            offset++;
+          }
+          else {
+            offset--;
+          }
+        }
+        setText(part);
+        count++
+        //37 script disappears
+        //25 keeps text on screen
+        if (count === 25) {
+          clearInterval(a)
+        }
+      }, speed);
+  }, [])
 
   const navigate = useNavigate();
 
