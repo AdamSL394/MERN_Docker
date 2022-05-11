d09fc25773ddff28c28eddfcf0413acbeafa040b
Adam <lehreradam@yahoo.com>
2022-05-11 10:49:15 -0700

    commenting out db connection

--- server/server.js
+++ server/server.js
@@ -57,17 +57,17 @@ if (process.env.NODE_ENV === "staging") {
 
 if (process.env.NODE_ENV === "production") {
     console.log("Host", process.env.NODE_ENV )
-    main().catch(err => console.log(err));
-    async function main() {
-        const connect = await mongoose.connect(`mongoose.connect("mongodb+srv://adam:notescript@cluster0.fepd2.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`, {
-            dbName: process.env.DB_NAME,
-            useNewUrlParser: true,
-            useCreateIndex: true,
-            useUnifiedTopology: true,
-            useFindAndModify: true,
-        })
-        console.log(`Staging MongoDB connected: ${(connect.connection.host)}`);
-    }
+    // main().catch(err => console.log(err));
+    // async function main() {
+    //     const connect = await mongoose.connect(`mongoose.connect("mongodb+srv://adam:notescript@cluster0.fepd2.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`, {
+    //         dbName: process.env.DB_NAME,
+    //         useNewUrlParser: true,
+    //         useCreateIndex: true,
+    //         useUnifiedTopology: true,
+    //         useFindAndModify: true,
+    //     })
+    //     console.log(`Staging MongoDB connected: ${(connect.connection.host)}`);
+    // }
 }
 
 //Research 
