a3d644f6ab3201107d371d349d1c2bf60e981e15
Adam <lehreradam@yahoo.com>
2022-05-11 17:35:40 -0700

    clean up

--- client/src/index.js
+++ client/src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App.js';
-import reportWebVitals from './reportWebVitals.js';
 import { Auth0Provider } from "@auth0/auth0-react";
 // import config from './auth_config.json'
 
@@ -24,4 +23,3 @@ root.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
