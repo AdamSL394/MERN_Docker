83e696e43704d90d63a75045a39d8105b78ff032
Adam <lehreradam@yahoo.com>
2022-05-12 12:33:04 -0700

    test setting enviorment

--- client/src/config/config.js
+++ client/src/config/config.js
@@ -1,5 +1,5 @@
 import config from './config.json'
-const enviroment = process.env.REACT_APP_HOST || 'development'
+const enviroment = process.env.REACT_APP_HOST || 'production'
 const enviromentAPI = config[enviroment]
 export default enviromentAPI 
 
