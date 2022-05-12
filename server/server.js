562e28a30a09f1a07dcfe9325f2eadf085be6804
Adam <lehreradam@yahoo.com>
2022-05-12 13:33:58 -0700

    trying to server front end as static js

--- server/server.js
+++ server/server.js
@@ -47,7 +47,7 @@ app.use(cors());
 
 if (process.env.NODE_ENV === "development" || process.env.NODE_ENV === "production" ) {
     console.log("build")
-    app.use(express.static(path.resolve(__dirname, '../client/build')));
+    app.use(express.static(path.join(__dirname, '../client/build')));
     app.get('/*', function (req, res) {
         res.sendFile(path.join(__dirname, '../client/build', "index.html"));
     });
