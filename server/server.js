7bc267acc8db8a700b3726ccb076504211f39ffe
Adam <lehreradam@yahoo.com>
2022-05-13 10:11:04 -0700

    test

--- server/server.js
+++ server/server.js
@@ -42,7 +42,10 @@ app.use(bodyParser.json())
 app.use(cors());
 
 app.use(express.static(path.join(__dirname, '/build', "index.html")));
-console.log(path.join(__dirname, "./build", "index.html"),"---------__S--------")
+app.get('/*', function (req, res) {
+    res.sendFile((__dirname, "./build", "index.html"));
+});
+console.log(res.sendFile(path.join(__dirname, "./build", "index.html")),"---------__S--------")
 if (process.env.NODE_ENV === "development" || process.env.NODE_ENV === "production" ) {
     console.log("build")
 
