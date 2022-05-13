40e1e33a5890793c32f9dc42f200410b06128572
Adam <lehreradam@yahoo.com>
2022-05-12 22:29:17 -0700

    test

--- server/server.js
+++ server/server.js
@@ -41,9 +41,11 @@ app.use(bodyParser.json())
 // research
 app.use(cors());
 
+app.use(express.static(path.join(__dirname, '../client/build')));
+
 if (process.env.NODE_ENV === "development" || process.env.NODE_ENV === "production" ) {
     console.log("build")
-    app.use(express.static(path.join(__dirname, '../client/build')));
+
     app.get('/*', function (req, res) {
         res.sendFile(path.join(__dirname, '../client/build', "index.html"));
     });
